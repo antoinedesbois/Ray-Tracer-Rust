84aca984db7e93819d1f2f193dc57189527442f8
Antoine Desbois <antoine.desbois@autodesk.com>
2017-10-17 10:12:23 -0400

    Implement BVH to speed up rendering

--- src/tracer/utils/mod.rs
+++ src/tracer/utils/mod.rs
@@ -4,9 +4,11 @@ pub mod intersection;
 pub mod scene;
 pub mod ray;
 pub mod camera;
+pub mod bounding_volume_hierarchy;
 
 pub use tracer::utils::ray::Ray;
 pub use tracer::utils::color::Color;
 pub use tracer::utils::scene::Scene;
 pub use tracer::utils::camera::Camera;
-
+pub use tracer::utils::bounding_volume_hierarchy::BoundingVolumeHierarchy;
+pub use tracer::utils::bounding_volume_hierarchy::HitInfo;
